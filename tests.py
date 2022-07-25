a71ad63ff599f2163185c3fabc2aa46dd2489ffa
Chimdi Chikezie <chikeziechimdi@gmail.com>
2022-07-25 00:15:55 -0700

    Wrote testNoLower to see if a string with valid length without a lowercase is rejected correctly

--- tests.py
+++ tests.py
@@ -12,5 +12,9 @@ class TestCase(unittest.TestCase):
         input = "1111144"
         self.assertFalse(check_pwd(input))
 
+    def testNoLower(self):
+        input = "111117777"
+        self.assertFalse(check_pwd(input))
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
